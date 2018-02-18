8d0146e3315e95961324065ed2f8282634212df0
Sergio Martín <sergiomartinmoreno@outlook.com>
2018-02-18 20:58:12 +0100

    Created MainMenu Map. Creating IU: step 3/3 => FINISHED Alpha MainMenu.

--- Source/SimonSays/SimonSays.Build.cs
+++ Source/SimonSays/SimonSays.Build.cs
@@ -8,7 +8,7 @@ public class SimonSays : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
